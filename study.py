9487512455159f726391390dc7af388256870343
Tijax Oxlajuj <tijaxmayaulew@gmail.com>
2026-04-08 20:13:37 -0300

    estoi probando el pq no me aparecen lines dentro de mi project on git

---
+++ study.py
@@ -0,0 +1,20 @@
+
+print("maitei mundore")
+# Cuenta del 1 al 5 (solo estoy probando el pq no consigo ver las lineas en github)
+for i in range(1, 6):
+    print(f"Número: {i}")
+
+#python practice 08/04/2026
+#current_time_str = input ("What is the current time (in hours 0 - 23)?")
+#wait_time_str = input ("How many hours do you want to wait")
+#current_time_int = int (current_time_str) 
+#wait_time_int = int(wait_time_int)  wait_time_int does not have a value so it cannot be used on the right hand side.
+#final_time_int = current_time_int + wait_time_int
+#print(final_time_int)   
+
+#present_time = input("Enter the present timein hours:")
+#set_time = input("Set the hours for alarm:")
+#present_time = int(present_time) 
+#set_time = int (set_time)
+#alarm_time = present_time + set_time
+#print(alarm_time)
